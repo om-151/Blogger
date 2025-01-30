696a73cba2e0ae85faf142f7e9ae954939962bfa
OM SONANI <143808904+om-151@users.noreply.github.com>
2025-01-30 09:04:52 +0530

    all set !!

--- server/server.js
+++ server/server.js
@@ -8,16 +8,26 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 app.use(morgan('dev'));
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = ["https://blogger-client-su4z.onrender.com"];
+        const isAllowed = allowedOrigins.includes(origin);
+        callback(null, isAllowed ? origin : false)
+    },
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // Routes
 app.use('/api/users', require('./routes/users'));
